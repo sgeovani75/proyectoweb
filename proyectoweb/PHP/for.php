e5517d7e9a6e4918535ecf2092bcaa37663aae68
Geovanni Simbaña <sgeovani75@hotmail.com>
2020-06-03 13:01:42 -0500

    proyecto con formulario

---
+++ proyectoweb/PHP/for.php
@@ -0,0 +1,19 @@
+<?php
+    $num1=5;
+    $num2=30;
+    $mul=0;
+   // $mul=$num1*$num2;
+    for ($i=1;$i<=$num1;$i++){
+
+        $mul=$mul+$num2;
+        echo "<br>";
+    }
+
+    echo "el resultado de la multiplicacion de $num1 y $num2 = $mul";
+    
+
+
+    
+?>
+
+    
---
+++ proyectoweb/PHP/procesa.php
@@ -0,0 +1,8 @@
+<?php
+    $num1=$_POST['numero1'];   
+    $num2=$_POST['numero2'];
+
+    echo "El valor de numero 1 es : $num1<br>";
+    echo "El valor de numero 1 es : $num2";
+?>
+
